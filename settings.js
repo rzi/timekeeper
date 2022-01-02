530d082f0be9634d5d3d936dd833ce743e44c595
rafalzietak <rzi@vp.pl>
2022-01-03 00:24:27 +0100

    working settings

--- settings.js
+++ settings.js
@@ -1,19 +1,27 @@
 let selectElement = document.getElementById("numberOfPresenters");
 let btnAddP = document.getElementById("btnAddP");
-let copyData = {};
+let copyData =[];
 btnAddP.addEventListener("click", function () {
   console.log("btn");
   console.log(`copyData: ${JSON.stringify(copyData)}`);
+  var nbOfObj =copyData.length+1
+  console.log(`copyDataLenght: ${nbOfObj}`);
+  const addName=document.getElementById("addName").value;
+  const setTime = document.getElementById("addTime").value; 
+  copyData.push({"id": nbOfObj,"name":addName, "active":true, "setTime":setTime })
+  console.log(`copyData2: ${JSON.stringify(copyData)}`);
+  //save to json
+  //refresh
 });
 readTextFile("./test.json", function (text) {
   var data = JSON.parse(text);
   console.log(`dane: ${JSON.stringify(data)}`);
   document.getElementById("numberOfPresenters").value = 5;
   console.log(`wartość ${selectElement.value}`);
-  const nbOfItems = data[0].nbOfItems;
+  const nbOfItems = data.length;
   console.log(`data0 ${nbOfItems}`);
   createPresenters(parseInt(nbOfItems), data);
-  copyData = Object.assign({}, data);
+  copyData = [...data];
 });
 selectElement.addEventListener("change", (event) => {
   console.log(`nb. of change  ${event.target.value}`);
@@ -27,11 +35,11 @@ selectElement.addEventListener("change", (event) => {
 function createPresenters(index, data) {
   for (i = 0; i < index; i++) {
     var para = document.createElement("p"); // Create a <p> node
-    var t = document.createTextNode(`id: ${data[i + 1].name}`); // Create a text node
-    para.id = String(data[i + 1].id);
+    var t = document.createTextNode(`id: ${data[i].name}`); // Create a text node
+    para.id = String(data[i ].id);
     para.appendChild(t); // Append the text to <p>
 
-    var t2 = document.createTextNode(` setTime: ${data[i + 1].setTime}`); // Create a text node
+    var t2 = document.createTextNode(` setTime: ${data[i].setTime}`); // Create a text node
     para.appendChild(t2); // Append the text to <p>
 
     document.getElementById("presenters").appendChild(para); // Append <p> to <div> with id="myDIV"
